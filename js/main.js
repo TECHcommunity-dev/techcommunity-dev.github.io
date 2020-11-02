4c54cce8999b21eff9b79dffdd4cdca83600983d
Sandeep <sandeep.lati@gmail.com>
2020-11-02 19:51:29 +0530

    Sandeep Commit

--- js/main.js
+++ js/main.js
@@ -32,14 +32,6 @@ console.log("sandeep");
 
 
 
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myrepos div").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
 
 //tabls
 function connectorTabs(tabName) {
